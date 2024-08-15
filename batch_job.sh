1f91f70ea9ff55772bafba7a53a633cf1d01dd3e
Artur Ganzha <arturganzha10@gmail.com>
2024-08-15 12:58:12 +0200

    empty

--- batch_job.sh
+++ batch_job.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-
-#SBATCH --job-name=test_gpu
-#SBATCH --partition=ai
+#!/bin/bash -l
+#SBATCH --job-name=adrl-baselines
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem-per-cpu=10M
-#SBATCH --time=00:5:00
+#SBATCH --ntasks=1
+#SBATCH --time=00:20:00
+#SBATCH --partition=ai
 #SBATCH --mail-user=artur.ganzha@stud.uni-hannover.de
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=test_serial-job_%j.out
 #SBATCH --error=test_serial-job_%j.err
 
+
 # Change to the directory where your script is located
 cd /bigwork/nhwpgana/adrl-project
 
 # Load the correct Python module
-module load GCC/10.3.0
-module load CMake/3.20.1
+module spider python  # Check for available versions
 module load python/3.12.4  # Load the correct Python version
 
 # If conda is installed and available
