991a2744874ed6b166c06cec44f7276f8be4165e
Artur Ganzha <arturganzha10@gmail.com>
2024-08-15 12:33:06 +0200

    changes

--- batch_job.sh
+++ batch_job.sh
@@ -3,22 +3,20 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=00:20:00
-#SBATCH --constraint=[skylake|haswell]
+#SBATCH --partition=ai         # Explicitly set the partition to 'ainlp'
 #SBATCH --mail-user=artur.ganzha@stud.uni-hannover.de
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output test_serial-job_%j.out
-#SBATCH --error test_serial-job_%j.err
-# Change to my work dir
-# SLURM_SUBMIT_DIR is an environment variable that automatically gets
-# assigned the directory from which you did submit the job. A batch job
-# is like a new login, so you'll initially be in your HOME directory.
-# So it's usually a good idea to first change into the directory you did # submit your job from.
+#SBATCH --output=test_serial-job_%j.out
+#SBATCH --error=test_serial-job_%j.err
+
+# Change to the directory from which the job was submitted
 cd $SLURM_SUBMIT_DIR
-# Load the modules you need, see corresponding page in the cluster documentation
+
+# Load the necessary module
 module load python/3.12.4
-source activate adrl-project-luis 
-# Start my serial app
-# srun is needed here only to create an entry in the accounting system,
-python baselines.py 
-# but you could also start your app without it here, since it's only serial. srun ./my_serial_app
-srun ./baselines.py
+
+# Activate the conda environment (use the correct syntax for your system)
+source activate adrl-project-luis || conda activate adrl-project-luis
+
+# Run the Python script without srun
+python baselines.py
