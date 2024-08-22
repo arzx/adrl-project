801e1be03a2fda0a39718d805ee6db9d860e909d
Artur Ganzha <arturganzha10@gmail.com>
2024-08-22 12:18:03 +0200

    batch job

--- batch_job.sh
+++ batch_job.sh
@@ -1,23 +1,25 @@
 #!/bin/bash -l
-#SBATCH --job-name=adrl-baselines
-#SBATCH --partition=ai-n001  # Partition for AI workloads
-#SBATCH --gres=gpu:a100:2
-#SBATCH --mem-per-cpu=10M
-#SBATCH --time=08:00:00  # Adjust time based on expected runtime
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH -J sac_door
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH -o slurm_outputs/slurm-%j.out
+#SBATCH --partition=tnt,ai
+#SBATCH --time=6:00:00
 #SBATCH --mail-user=artur.ganzha@stud.uni-hannover.de
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=test_serial-job_%j.out
 #SBATCH --error=test_serial-job_%j.err
 
 # Change to the directory where your script is located
-cd /bigwork/nhwpgana/adrl-project
+module load Miniconda3
 
-# Load the correct Python module
-module spider python  # Check for available versions
-module load python/3.12.4  # Load the correct Python version
+source /home/nhwpgana/.bashrc
+conda activate /bigwork/nhwpgana/.conda/envs/adrl_project
+export WANDB_MODE=offline
 
-# Activate your conda environment
-source activate adrl-project-luis || conda activate adrl-project-luis
 
 # Run the Python script
-python src/agents/baselines.py --steps 1000000
+python bigwork/adrl-project/src/agents/baselines.py --steps 1000000
