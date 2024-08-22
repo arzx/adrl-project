54e4c5b5df52aa4dc74fa52c44b6a55c437d5425
Artur Ganzha <arturganzha10@gmail.com>
2024-08-22 12:38:17 +0200

    aaaa

--- batch_job.sh
+++ batch_job.sh
@@ -1,17 +1,17 @@
-#!/bin/bash -l
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH -J sac_door
+#SBATCH -J baselines
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
-#SBATCH -o slurm_outputs/slurm-%j.out
 #SBATCH --partition=tnt,ai
 #SBATCH --time=6:00:00
 #SBATCH --mail-user=artur.ganzha@stud.uni-hannover.de
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=test_serial-job_%j.out
 #SBATCH --error=test_serial-job_%j.err
+
+cd adrl-project/slurm_outputs/
 
 # Change to the directory where your script is located
 module load Miniconda3
