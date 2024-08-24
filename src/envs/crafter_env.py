08d8165a2dec1aaf47e2f066964f5e137ee16467
Artur Ganzha <arturganzha10@gmail.com>
2024-08-24 19:08:52 +0200

    env fix

--- src/envs/crafter_env.py
+++ src/envs/crafter_env.py
@@ -1,70 +1,26 @@
-from craftax.craftax.envs.craftax_pixels_env import (
-    CraftaxPixelsEnv,
-    CraftaxPixelsEnvNoAutoReset,
+import argparse
+import crafter
+import stable_baselines3
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+parser = argparse.ArgumentParser()
+parser.add_argument('--outdir', default='logdir/crafter_reward-ppo/0')
+parser.add_argument('--steps', type=float, default=1e6)
+args = parser.parse_args()
+
+env = crafter.Env()
+
+def create_env():
+    """
+    Create and return the Crafter environment.
+    """
+    env = crafter.Env()
+    return env
+env = create_env()
+model = stable_baselines3.PPO('CnnPolicy', env, verbose=1)
+env = crafter.Recorder(
+    env, args.outdir,
+    save_stats=False,
+    save_episode=False,
+    save_video=True,
 )
-from craftax.craftax.envs.craftax_symbolic_env import (
-    CraftaxSymbolicEnv,
-    CraftaxSymbolicEnvNoAutoReset,
-)
-from craftax.craftax_classic.envs.craftax_pixels_env import (
-    CraftaxClassicPixelsEnv,
-    CraftaxClassicPixelsEnvNoAutoReset,
-)
-from craftax.craftax_classic.envs.craftax_symbolic_env import (
-    CraftaxClassicSymbolicEnv,
-    CraftaxClassicSymbolicEnvNoAutoReset,
-)
-
-
-def make_craftax_env_from_name(name: str, auto_reset: bool):
-    if auto_reset:
-        if name == "Craftax-Symbolic-v1" or name == "Craftax-Symbolic-AutoReset-v1":
-            return CraftaxSymbolicEnv()
-        elif name == "Craftax-Pixels-v1" or name == "Craftax-Pixels-AutoReset-v1":
-            return CraftaxPixelsEnv()
-        if (
-            name == "Craftax-Classic-Symbolic-v1"
-            or name == "Craftax-Classic-Symbolic-AutoReset-v1"
-        ):
-            return CraftaxClassicSymbolicEnv()
-        elif (
-            name == "Craftax-Classic-Pixels-v1"
-            or name == "Craftax-Classic-Pixels-AutoReset-v1"
-        ):
-            return CraftaxClassicPixelsEnv()
-    else:
-        if name == "Craftax-Symbolic-v1":
-            return CraftaxSymbolicEnvNoAutoReset()
-        elif name == "Craftax-Pixels-v1":
-            return CraftaxPixelsEnvNoAutoReset()
-        elif name == "Craftax-Classic-Symbolic-v1":
-            return CraftaxClassicSymbolicEnvNoAutoReset()
-        elif name == "Craftax-Classic-Pixels-v1":
-            return CraftaxClassicPixelsEnvNoAutoReset()
-
-    raise ValueError(f"Unknown craftax environment: {name}")
-
-
-def make_craftax_env_from_params(classic: bool, symbolic: bool, auto_reset: bool):
-    if classic:
-        if symbolic:
-            if auto_reset:
-                return CraftaxClassicSymbolicEnv()
-            else:
-                return CraftaxClassicSymbolicEnvNoAutoReset()
-        else:
-            if auto_reset:
-                return CraftaxClassicPixelsEnv()
-            else:
-                return CraftaxClassicPixelsEnvNoAutoReset()
-    else:
-        if symbolic:
-            if auto_reset:
-                return CraftaxSymbolicEnv()
-            else:
-                return CraftaxSymbolicEnvNoAutoReset()
-        else:
-            if auto_reset:
-                return CraftaxPixelsEnv()
-            else:
-                return CraftaxPixelsEnvNoAutoReset()
+model.learn(total_timesteps=args.steps)
